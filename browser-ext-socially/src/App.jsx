ce4b47d6e5db16ce19d44cfd5d9db385df9d45db
Sara Gibbons <saragibby@gmail.com>
2024-11-03 23:28:34 +0000

    inital upload of social browser extension project

---
+++ browser-ext-socially/src/App.jsx
@@ -0,0 +1,18 @@
+/**
+ * Application component
+ *
+ * To contain application wide settings, routes, state, etc.
+ */
+
+import React from "react";
+
+function App() {
+    return (
+        <div id="main">
+            <h2>Socially</h2>
+            <p>Generate a social media post for this page</p>
+        </div>
+    )
+}
+
+export default App;
